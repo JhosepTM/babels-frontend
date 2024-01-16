cc7cbac08f7a558d3d6c701df309430ddae5264f
Moritakha <92400582+Moritakha@users.noreply.github.com>
2024-01-16 15:45:47 -0500

    Creando el login para la pagina Frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { BookingPage } from './pages/BookingPage/BookingPage';
-import Home from './pages/Home';
-import Menu from './pages/Menu';
+import Home from './pages/LoginPage/Home';
+import Menu from './pages/LoginPage/Menu';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
