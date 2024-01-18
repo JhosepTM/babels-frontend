4494e1c0aa3021d44b0cf80c5cdf0d4ab5e3a9d0
Moritakha <92400582+Moritakha@users.noreply.github.com>
2024-01-18 15:20:19 -0500

    Imagenes como botones que envia a otra ruta

--- src/pages/MenuPage/MenuPage.tsx
+++ src/pages/MenuPage/MenuPage.tsx
@@ -8,20 +8,26 @@ const MenuPage = () => {
         <div className="center-container flex flex-col items-center justify-center h-full">
           {/* Imágenes arriba */}
           <div className="mb-4 flex items-center">
-            <div className="mr-4 text-center" style={{ marginBottom: '50px' }}>
-              <img src=".\src\assets\images\Estadisticas.svg" alt="Imagen 1" style={{ width: '200px', height: '170px' }} />
-              <p className="text-white font-bold mt-1">Estadisticas y Estudios</p>
+            <div className="mr-4 text-center menu-item hover:scale-105" style={{ marginBottom: '50px' }}>
+              <a href="/ruta-de-estadisticas">
+                <img src=".\src\assets\images\Estadisticas.svg" alt="Imagen 1" style={{ width: '200px', height: '170px' }} />
+                <p className="text-white font-bold mt-1">Estadisticas y Estudios</p>
+              </a>
             </div>
-            <div className="ml-4 text-center" style={{ marginBottom: '50px' }}>
-              <img src=".\src\assets\images\Reservas.svg" alt="Imagen 2" style={{ width: '300px', height: '150px', objectFit: 'cover' }} />
-              <p className="text-white font-bold mt-1">Reservar Habitaciones</p>
+            <div className="ml-4 text-center menu-item hover:scale-105" style={{ marginBottom: '50px' }}>
+              <a href="/ruta-de-reservas">
+                <img src=".\src\assets\images\Reservas.svg" alt="Imagen 2" style={{ width: '300px', height: '150px', objectFit: 'cover' }} />
+                <p className="text-white font-bold mt-1">Reservar Habitaciones</p>
+              </a>
             </div>
           </div>
 
           {/* Imagen abajo */}
-          <div className="items-center text-center" style={{ marginBottom: '50px' }}>
-            <img src=".\src\assets\images\EditAndAddRooms.svg" alt="Imagen 3" style={{ width: '250px', height: '170px', objectFit: 'cover' }} />
-            <p className="text-white font-bold mt-1">Administrar Habitaciones</p>
+          <div className="items-center text-center menu-item hover:scale-105" style={{ marginBottom: '50px' }}>
+            <a href="/ruta-de-administracion">
+              <img src=".\src\assets\images\EditAndAddRooms.svg" alt="Imagen 3" style={{ width: '250px', height: '170px', objectFit: 'cover' }} />
+              <p className="text-white font-bold mt-1">Administrar Habitaciones</p>
+            </a>
           </div>
         </div>
       </div>
