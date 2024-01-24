8c961f2de98c52a726148be5e958cc384d12520c
Moritakha <92400582+Moritakha@users.noreply.github.com>
2024-01-24 15:10:17 -0500

    Jugando con los tamaños de las fichas y de la imagen para que quede iguales (Responsive)

--- src/pages/RoomsPage/RoomsPage.tsx
+++ src/pages/RoomsPage/RoomsPage.tsx
@@ -1,29 +1,26 @@
 import Header from '@/components/ui/Header';
-import "@/assets/images/img1.jpeg";
 
 const RoomsPage = () => {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-screen bg-gray-950">
       <Header showBackButton backRoute="/madmin" showAddRoomButton={true} />
-
       <div className="flex justify-center items-center mt-8">
-        <div className="flex rounded-lg shadow-lg bg-zinc-800 w-96">
+        <div className="flex rounded-lg shadow-lg bg-zinc-800 w-2/4 max-h-72 overflow-hidden">
           {/* Contenedor de la imagen y texto */}
           <div className="flex">
             {/* Contenedor de la imagen (lado izquierdo) */}
-            <div className="flex-shrink-0">
+            <div className="flex-shrink-0 w-2/5 h-auto max-h-72">
               <img
                 src="/src/assets/images/img1.jpeg"
                 alt="Imagen 1"
-                className="w-48 h-48 object-cover rounded-lg"
                 style={{ objectFit: 'cover' }}
               />
             </div>
 
             {/* Contenedor del texto (lado derecho) */}
             <div className="p-8">
-              <h2 className="text-white font-bold mb-4">Título de la Ficha</h2>
-              <p className="text-white mb-4">Descripción de la ficha.</p>
+              <h2 className="text-white font-bold mb-4">Cuarto del Amor</h2>
+              <p className="text-white mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec massa euismod, accumsan velit vitae, sagittis nibh. Pellentesque aliquam nisl massa, ut lobortis tortor egestas a. Donec laoreet risus a nisl efficitur, a porta ligula egestas.</p>
             </div>
           </div>
         </div>
