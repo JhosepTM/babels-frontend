bc25c34bddc97fc36d650ab86026c3f418596b90
Moritakha <92400582+Moritakha@users.noreply.github.com>
2024-01-26 14:23:05 -0500

    Imagen ajustada y botones añadidos al contenedor de las habitaciones

--- src/pages/RoomsPage/RoomsPage.tsx
+++ src/pages/RoomsPage/RoomsPage.tsx
@@ -9,18 +9,26 @@ const RoomsPage = () => {
           {/* Contenedor de la imagen y texto */}
           <div className="flex">
             {/* Contenedor de la imagen (lado izquierdo) */}
-            <div className="flex-shrink-0 w-2/5 h-auto max-h-72">
+            <div className="flex-shrink-0 w-2/5">
               <img
                 src="/src/assets/images/img1.jpeg"
                 alt="Imagen 1"
-                style={{ objectFit: 'cover' }}
+                style={{ objectFit: 'cover', width: '100%', height: '100%' }}
               />
             </div>
 
-            {/* Contenedor del texto (lado derecho) */}
-            <div className="p-8">
-              <h2 className="text-white font-bold mb-4">Cuarto del Amor</h2>
-              <p className="text-white mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec massa euismod, accumsan velit vitae, sagittis nibh. Pellentesque aliquam nisl massa, ut lobortis tortor egestas a. Donec laoreet risus a nisl efficitur, a porta ligula egestas.</p>
+            {/* Contenedor del texto y botones (lado derecho) */}
+            <div className="p-8 flex flex-col justify-between">
+              <div>
+                <h2 className="text-white font-bold mb-4">Cuarto del Amor</h2>
+                <p className="text-white mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec massa euismod, accumsan velit vitae, sagittis nibh. Pellentesque aliquam nisl massa, ut lobortis tortor egestas a. Donec laoreet risus a nisl efficitur, a porta ligula egestas.</p>
+              </div>
+              <div className="flex space-x-4">
+                {/* Botón Eliminar */}
+                <button className="bg-blue-500 text-white px-4 py-2 rounded">Editar</button>
+                {/* Botón Editar */}
+                <button className="bg-red-500 text-white px-4 py-2 rounded">Eliminar</button>
+              </div>
             </div>
           </div>
         </div>
