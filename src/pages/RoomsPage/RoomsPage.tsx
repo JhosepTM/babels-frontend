f051065b48e9a5b55c6c689f48473bca672f5735
Moritakha <92400582+Moritakha@users.noreply.github.com>
2024-01-29 10:22:24 -0500

    Realizando configuraciones para que el texto y la imagen sean responsives en cuanto el ancho de pantalla

--- src/pages/RoomsPage/RoomsPage.tsx
+++ src/pages/RoomsPage/RoomsPage.tsx
@@ -1,6 +1,27 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import Header from '@/components/ui/Header';
 
 const RoomsPage = () => {
+  // Lista de rutas de imágenes
+  const imagePaths = [
+    '/src/assets/images/img1.jpeg',
+    '/src/assets/images/img2.jpeg',
+    '/src/assets/images/img3.jpeg',
+    // Agrega más rutas de imágenes según sea necesario
+  ];
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handlePrevImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? imagePaths.length - 1 : prevIndex - 1));
+  };
+
+  const handleNextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex === imagePaths.length - 1 ? 0 : prevIndex + 1));
+  };
+
   return (
     <div className="flex flex-col h-screen bg-gray-950">
       <Header showBackButton backRoute="/madmin" showAddRoomButton={true} />
@@ -9,12 +30,26 @@ const RoomsPage = () => {
           {/* Contenedor de la imagen y texto */}
           <div className="flex">
             {/* Contenedor de la imagen (lado izquierdo) */}
-            <div className="flex-shrink-0 w-2/5">
+            <div className="flex-shrink-0 w-2/5 relative">
               <img
-                src="/src/assets/images/img1.jpeg"
-                alt="Imagen 1"
+                src={imagePaths[currentImageIndex]}
+                alt={`Imagen ${currentImageIndex + 1}`}
                 style={{ objectFit: 'cover', width: '100%', height: '100%' }}
               />
+              {/* Flecha izquierda */}
+              <button
+                className="absolute top-1/2 left-0 transform -translate-y-1/2 text-white bg-transparent p-4 rounded-full hover:bg-gray-700 focus:outline-none"
+                onClick={handlePrevImage}
+              >
+                <FontAwesomeIcon icon={faChevronLeft} size="2x" />
+              </button>
+              {/* Flecha derecha */}
+              <button
+                className="absolute top-1/2 right-0 transform -translate-y-1/2 text-white bg-transparent p-4 rounded-full hover:bg-gray-700 focus:outline-none"
+                onClick={handleNextImage}
+              >
+                <FontAwesomeIcon icon={faChevronRight} size="2x" />
+              </button>
             </div>
 
             {/* Contenedor del texto y botones (lado derecho) */}
@@ -25,9 +60,9 @@ const RoomsPage = () => {
               </div>
               <div className="flex space-x-4">
                 {/* Botón Eliminar */}
-                <button className="bg-blue-500 text-white px-4 py-2 rounded">Editar</button>
+                <button className="bg-gray-950 text-white px-4 py-2 rounded transition duration-300 ease-in-out transform hover:scale-105">Editar</button>
                 {/* Botón Editar */}
-                <button className="bg-red-500 text-white px-4 py-2 rounded">Eliminar</button>
+                <button className="bg-gray-950 text-white px-4 py-2 rounded transition duration-300 ease-in-out transform hover:scale-105">Eliminar</button>
               </div>
             </div>
           </div>
